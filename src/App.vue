f908c25e24b9456d20f60ef7e76a096e38cb0fc3
Kobe Cao <1957365020@qq.com>
2020-12-22 20:44:50 +0800

    学习cors跨域

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,31 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>{{age}}</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
+import axios from 'axios'
+// import jsonp from 'jsonp'
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: "APP",
+  compomemts: {
+    
+  },
+  data() {
+    return {
+      age: 20
+    }
+  },
+  mounted() {
+    let url = "https://live.imooc.com/project/livedetaillist?actionType=online"
+    axios.get(url).then(()=> {
+
+    })
+    // jsonp
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
