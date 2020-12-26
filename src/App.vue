37a53376e5f3f7f12ab688a99f7ebf69c77d0da8
Kobe Cao <1957365020@qq.com>
2020-12-26 21:29:43 +0800

    Mock设置

--- src/App.vue
+++ src/App.vue
@@ -16,10 +16,18 @@ export default {
     }
   },
   mounted() {
-    // 本地加载请求静态资源json文件的形式。
-    this.axios.get('/mock/user/login.json').then((res)=>{
+    // 1.本地加载请求静态资源json文件的形式。
+    // this.axios.get('/mock/user/login.json').then((res)=>{
+    //   this.res = res;
+    // })
+    // 2.通过easy-mock平台实现数据Mock
+    // this.axios.get('/user/login').then((res)=>{
+    //   this.res = res;
+    // });
+    // 3.本地集成mockjs实现数据mock
+    this.axios.get('/user/login').then((res)=>{
       this.res = res;
-    })
+    });
   }
 }
 </script>
